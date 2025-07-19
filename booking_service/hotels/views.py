cf6c813d6b06d38898cd55d88f2e436724b18cd4
Vladimir <vov.voron2005@gmail.com>
2025-07-19 21:26:01 +0300

    fix: validation booking validation error

--- booking_service/hotels/views.py
+++ booking_service/hotels/views.py
@@ -6,38 +6,40 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from hotels.models import Room, Booking
 from hotels.serializers import RoomSerializer, BookingSerializer
+from django.shortcuts import get_object_or_404
 
 
-class RoomViewSet(
+class BaseCreateDeleteViewSet(
     mixins.CreateModelMixin,
     mixins.DestroyModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ["price", "created_at"]
-    ordering = ["-created_at"]
-
     def create(self, request: Request, *args, **kwargs) -> Response:
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         instance = serializer.save()
         return Response({"id": instance.id}, status=status.HTTP_201_CREATED)
 
-    def delete(self, request: Request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
+    def destroy(self, request: Request, *args, **kwargs) -> Response:
+        return super().destroy(request, *args, **kwargs)
 
 
-class BookingViewSet(
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
+class RoomViewSet(BaseCreateDeleteViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        Room.price.field.name,
+        Room.created_at.field.name
+    ]
+    ordering = [f"-{Room.created_at.field.name}"]
+
+
+class BookingViewSet(BaseCreateDeleteViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    ordering = [Booking.start_date.field.name]
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -47,17 +49,9 @@ class BookingViewSet(
             raise serializers.ValidationError("Room parameter (room or room_id) is required")
 
         try:
-            room_id = int(room_id)
-        except (TypeError, ValueError):
-            raise serializers.ValidationError("Room ID must be a valid integer")
-
-        return queryset.filter(room_id=room_id)
-
-    def create(self, request: Request, *args, **kwargs) -> Response:
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
-        return Response({"id": instance.id}, status=status.HTTP_201_CREATED)
-
-    def delete(self, request: Request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
+            get_object_or_404(Room, id=int(room_id))
+            return queryset.filter(room_id=room_id)
+        except ValueError:
+            raise serializers.ValidationError(
+                {"room": "Room ID must be a valid integer"}
+            )
