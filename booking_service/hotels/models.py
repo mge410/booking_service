851c7c8a04971849947b26b8b5fd8a9360bfd06b
Vladimir <vov.voron2005@gmail.com>
2025-07-28 17:09:59 +0300

    feat: add updated at to models

--- booking_service/hotels/models.py
+++ booking_service/hotels/models.py
@@ -1,8 +1,23 @@
 from django.db import models
-from django.utils import timezone
 
 
-class Room(models.Model):
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(
+        "date of creation",
+        auto_now_add=True,
+        help_text="Date of creation",
+    )
+    updated_at = models.DateTimeField(
+        "date of update",
+        auto_now=True,
+        help_text="Date of update",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Room(TimeStampMixin):
     objects = models.Manager()
     description = models.TextField("description", db_column="description")
     price = models.DecimalField(
@@ -11,14 +26,9 @@ class Room(models.Model):
         max_digits=6,
         help_text="price per night at the room in dollars",
     )
-    created_at = models.DateTimeField(
-        "date of creation",
-        default=timezone.now,
-        help_text="Date of creation",
-    )
 
 
-class Booking(models.Model):
+class Booking(TimeStampMixin):
     room = models.ForeignKey(
         Room,
         on_delete=models.CASCADE,
